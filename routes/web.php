70676943be0f20bb11cc4fa63c4a3c55bfcc7569
quanbaphuongnam <nam.qbp29@aptecchlearning.edu.vn>
2021-03-19 07:07:09 +0700

    add folder

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ Route::get('/', function () {
 });
 
 Route::get('/admin', function () {
-    return view('backend.home');
+    return view('backend.admin');
 })->name('admin');
 
 Route::get('/index', function () {
